ffd780ddfb9fced37a25f12bc24698dea05386df
dayolian <daykenzie@gmail.com>
2026-04-14 13:39:41 -0700

    small changes for setup, addes to gitignore

---
+++ ctx_downloader_rqc.py
@@ -0,0 +1,38 @@
+# Function to provide specified CTX image from PDS.  Scrapes
+# asu viewer for supplemental metadata before download.
+#
+# Written by Jonathan Sneed, 6-15-2019, in association with
+# GALE labs at the UCLA department of Earth, Planetary, and
+# Space Sciences.
+
+import csv
+import os
+import sys
+import requests
+import urllib
+import webbrowser
+import time
+
+def ctx_downloader_rqc(ctxId):
+
+	# Assemble url structure
+	ctxurl1 = 'https://image.mars.asu.edu/stream/'
+	ctxurl2 = '.tiff?image=/mars/images/ctx/'
+	ctxurl3 = '/prj_full/'
+
+	# Retrieve the volume ID from both pages to fill out geotiff url
+	fp = urllib.urlopen('https://viewer.mars.asu.edu/viewer/ctx/' + ctxId)
+	page = fp.read()
+	idspot = str(page).find('mrox_')
+	volid = str(page)[idspot:idspot+9]
+
+	# Construct URL	
+	url = ctxurl1 + ctxId + ctxurl2 + volid + ctxurl3 + ctxId + '.tiff'
+	
+	# Begin download proper
+	print('Dowloading:')
+	print(url)
+
+	webbrowser.open(url)
+	time.sleep(5)
+	# 20 second sleep works well but is slow 
